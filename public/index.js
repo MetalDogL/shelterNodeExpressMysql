30d5a674ec9ab01e9ea4f75fe832bb61033a8e6c
mark.kovari <mark.kovari@greenfoxacademy.com>
2019-05-30 19:16:30 +0200

    feat(fe): renders the table

--- public/index.js
+++ public/index.js
@@ -4,7 +4,28 @@ window.onload = () => {
     getDogs.open('GET', 'http://localhost:3021/getDogs', true);
     getDogs.onload = (response) => {
         if (response.target.status === 200) {
-            console.log(response.target.response)
+            let dogs = JSON.parse(response.target.response);
+            dogs.forEach(dog => {
+                let row = document.createElement('tr');
+                let name = document.createElement('td');
+                name.textContent = dog.name;
+                row.appendChild(name);
+
+                let age = document.createElement('td');
+                age.textContent = dog.age;
+                row.appendChild(age);
+
+                let date = document.createElement('td');
+                date.textContent = dog.date;
+                row.appendChild(date);
+
+                let hasHome = document.createElement('td');
+                hasHome.textContent = dog.hashome === 1 ? "Yes" : "NO";
+                row.appendChild(hasHome);
+
+                toFill.appendChild(row);
+            })
+            console.log(dogs)
         }
 
     }
